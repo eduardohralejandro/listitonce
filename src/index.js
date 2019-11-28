66920cbe75837a6a41e835e8f32f17c1e068c2f9
Eduardo Hernandez <eduardo@chatbot-plus.com>
2019-11-28 14:41:40 +0100

    data model for lists without items

--- src/index.js
+++ src/index.js
@@ -1,18 +1,48 @@
 import { GraphQLServer } from 'graphql-yoga';
+import uuid from 'uuid';
+import db from './db';
 
 
 const typeDefs = `
 type Query {
-  listTitle: String!
+    lists: [List!]!
+}
+
+type Mutation {
+    createList(title: String!): List
+    deleteList(id: ID!): List
+}
+
+type List {
+    id: ID!
+    title: String!
 }
 `
 
 const resolvers = {
     Query: {
-        listTitle: () => {
-            return 'Dinner';
-        }
+        lists: (parent, args, _, info) => {
+            return db.list
+        },
     },
+    Mutation: {
+        createList: (parent, args, _, info) => {
+            const newList = {
+                id: uuid(),
+                ...args
+            }
+            db.list.push(newList);
+            return newList;
+        },
+        deleteList: (parent, args, db, info) => {
+            const listIndex = db.list.findIndex((list) => list.id === args.id);
+            if (listIndex > -1) {
+                const deleted = db.list.splice(listIndex, 1)
+                return deleted[0]
+            }
+            return null
+        }
+    }
 };
 
 const server = new GraphQLServer({
