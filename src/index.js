2b85cae3ebbf74b3a50c278bf7812c935db0940e
Eduardo Hernandez <eduardo@chatbot-plus.com>
2019-11-29 14:54:36 +0100

    new schema folder structure

--- src/index.js
+++ src/index.js
@@ -1,54 +1,20 @@
 import { GraphQLServer } from 'graphql-yoga';
-import uuid from 'uuid';
 
 import db from './db';
+import Mutation from './resolvers/Mutation';
+import Query from './resolvers/Query';
 
-
-const typeDefs = `
-type Query {
-    lists: [List!]!
-}
-
-type Mutation {
-    createList(title: String!): List
-    deleteList(id: ID!): List
-}
-
-type List {
-    id: ID!
-    title: String!
-}
-`
 
 const resolvers = {
-    Query: {
-        lists: (parent, args, _, info) => {
-            return db.list
-        },
-    },
-    Mutation: {
-        createList: (parent, args, _, info) => {
-            const newList = {
-                id: uuid(),
-                ...args
-            }
-            db.list.push(newList);
-            return newList;
-        },
-        deleteList: (parent, args, db, info) => {
-            const listIndex = db.list.findIndex((list) => list.id === args.id);
-            if (listIndex > -1) {
-                const deleted = db.list.splice(listIndex, 1)
-                return deleted[0]
-            }
-            return null
-        }
-    }
-};
-
+    Mutation,
+    Query
+}
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers,
+    context: {
+        db
+    },
 });
 
 
