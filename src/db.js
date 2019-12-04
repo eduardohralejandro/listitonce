b86edf9a73b9ed9cb840784e1f28cd67a1e95f78
Eduardo Hernandez <eduardo@chatbot-plus.com>
2019-12-04 17:03:12 +0100

    new schema structure for graphql

--- src/db.js
+++ src/db.js
@@ -1,48 +1,10 @@
 import uuid from 'uuid';
 
 
-const list = [{
-  listTitle: 'super title',
-  id: uuid(),
-  items: [
-    {
-      id: uuid(),
-      title: 'super tangent',
-      employee: 'Eduardo',
-      product: 'Chicken',
-      bought: false,
-    },
-    {
-      id: uuid(),
-      title: 'new hypothesis',
-      employee: 'Eddy',
-      product: 'Fish',
-      bought: false
-    },
-  ]
-}]
+const list = [{items: []}];
 
 
-const shoppingList = [{
-  listTitle: 'first shop list',
-  id: uuid(),
-  items: [
-    {
-      id: uuid(),
-      title: 'dinner',
-      employee: 'Eduardo',
-      product: 'pan',
-      bought: false,
-    },
-    {
-      id: uuid(),
-      title: 'yep',
-      employee: 'Eddy',
-      product: 'hehehe',
-      bought: false
-    },
-  ]
-}];
+const shoppingList = [];
 
 const db = {
   list,
