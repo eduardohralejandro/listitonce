27f3da5aceffcf33716e5a74d6093e957f53d74e
Eduardo Hernandez <eduardo@chatbot-plus.com>
2019-12-11 13:04:31 +0100

    read operation from database to have a complete query read action

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,8 +2,17 @@ const Query = {
     list: (parent, args, { db }, info) => {
         return db.list;
     },
-    savedList: (parent, args, { db }, info) => {
-        return db.shoppingList;
+    savedList: (parent, args, { db, ListSchema }, info) => {
+        const lists = ListSchema.find({}, (error, response) => {
+            try {
+                return response;
+            }
+            catch (e) {
+                throw new Error(error);
+            }
+        });
+
+        return lists;
     }
 }
 
