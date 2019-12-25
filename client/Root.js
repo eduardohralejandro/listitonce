8403f02feb93e5c3c06c22d2defbfd83aa85fbf0
Eduardo Hernandez <eduardo@chatbot-plus.com>
2019-12-25 16:47:01 +0100

    new component + router recipes

--- client/Root.js
+++ client/Root.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { Provider, createClient, dedupExchange, fetchExchange,  } from 'urql';
 import { cacheExchange } from '@urql/exchange-graphcache';
-import {
-    BrowserRouter,
-    Switch,
-    Route,
-  } from "react-router-dom";
 
 import UserLayout from './components/user_layout/UserLayout';
-import Navbar from './components/navbar/Navbar';
-import Recipes from './components/recipes/Recipes';
 import GET_LISTS from './components/user_layout/GET_LISTS.graphql';
 import RENDER_LISTS from './components/render_lists/RENDER_LISTS.graphql';
 
@@ -67,15 +60,9 @@ const client = createClient({
 
 const Root = () => {
     return (
-        <BrowserRouter>
-            <Provider value={client}>
-                <Navbar />
-                <Switch>
-                    <Route exact path="/recipes"  component={Recipes} />
-                    <Route exact path="/"  component={UserLayout} />
-                </Switch>
-            </Provider>
-        </BrowserRouter>
+        <Provider value={client}>
+            <UserLayout />
+        </Provider>
     );
 };
 
