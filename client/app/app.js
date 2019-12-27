7155b7d68f6a047d2e423d0cfd264ed928097388
Eduardo Hernandez <eduardo@chatbot-plus.com>
2019-12-27 11:21:34 +0100

    fetch data without any error on component GetProduct, load correctly the data needed to pass it to select option-ingrdients

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { hot } from 'react-hot-loader';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Root from '../Root';
+
 
 let App = () => {
     return (
